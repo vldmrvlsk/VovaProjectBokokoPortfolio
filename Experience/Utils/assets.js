90c0e053a7f9f05ebfe4a10ac153b0f057dd3027
vldmrvlsk <vlasyuk@edu.surgu.ru>
2022-11-14 17:11:15 +0500

    ALL DONE

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        name: "room",
+        type: "glbModel",
+        path: "/models/roomy.glb",
+    },
+
+    {
+        name: "screen",
+        type: "videoTexture",
+        path: "/textures/water.mp4",
+    }
+];
